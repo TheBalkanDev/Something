c1f7519195467c5f65637ace1ed0961c85f16727
BalkanDev <75144786+TheBalkanDev@users.noreply.github.com>
2021-08-28 18:40:51 +0200

    Add files via upload

---
+++ src/jasonwynn10/VanillaEntityAI/entity/neutral/DragonFireball.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+namespace jasonwynn10\VanillaEntityAI\entity\neutral;
+
+use pocketmine\entity\projectile\Projectile;
+
+class DragonFireball extends Projectile {
+}
---
+++ src/jasonwynn10/VanillaEntityAI/entity/neutral/Item.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+namespace jasonwynn10\VanillaEntityAI\entity\neutral;
+
+use jasonwynn10\VanillaEntityAI\entity\Collidable;
+use jasonwynn10\VanillaEntityAI\entity\CollisionCheckingTrait;
+use jasonwynn10\VanillaEntityAI\entity\CreatureBase;
+use pocketmine\block\Block;
+use pocketmine\entity\Entity;
+use pocketmine\entity\object\ItemEntity;
+
+class Item extends ItemEntity implements Collidable {
+	use CollisionCheckingTrait;
+
+	public function entityBaseTick(int $tickDiff = 1) : bool {
+		foreach($this->level->getNearbyEntities($this->boundingBox->expandedCopy(0.5,0.5,0.5), $this) as $entity) {
+			if($this->pickupDelay === 0 and $entity instanceof Item and $entity->onGround and $this->motion->equals($entity->getMotion()) and $this->item->equals($entity->getItem()) and ($count = $this->item->getCount() + $entity->getItem()->getCount()) <= $this->item->getMaxStackSize()) {
+				$this->item->setCount($count);
+				$entity->flagForDespawn();
+				foreach($this->getViewers() as $player)
+					$this->sendSpawnPacket($player);
+				break;
+			}
+		}
+		return parent::entityBaseTick($tickDiff);
+	}
+
+	public function onCollideWithEntity(Entity $entity) : void {
+		//TODO: minecart interactions
+	}
+
+	public function onCollideWithBlock(Block $block) : void {
+		// TODO: hoppers, pressure plates, tripwire
+	}
+
+	/**
+	 * @param Collidable $source
+	 */
+	public function push(Collidable $source) : void { // cannot be pushed
+	}
+}
