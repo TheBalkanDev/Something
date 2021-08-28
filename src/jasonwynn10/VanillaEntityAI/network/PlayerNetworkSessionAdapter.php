32bf1aa60d9b17ae27873b65694a29c1fc24dd33
BalkanDev <75144786+TheBalkanDev@users.noreply.github.com>
2021-08-28 17:39:21 +0200

    Create PlayerNetworkSessionAdapter.php

---
+++ src/jasonwynn10/VanillaEntityAI/network/PlayerNetworkSessionAdapter.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace jasonwynn10\VanillaEntityAI\network;
+
+use jasonwynn10\VanillaEntityAI\entity\passiveaggressive\Player;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\DataPacket;
+use pocketmine\network\mcpe\protocol\PlayerInputPacket;
+use pocketmine\Server;
+use pocketmine\timings\Timings;
+
+class PlayerNetworkSessionAdapter extends \pocketmine\network\mcpe\PlayerNetworkSessionAdapter {
+	/** @var Server */
+	protected $server;
+	/** @var Player */
+	protected $player;
+
+	public function __construct(Server $server, Player $player) {
+		parent::__construct($server, $player);
+		$this->server = $server;
+		$this->player = $player;
+	}
+
+	public function handlePlayerInput(PlayerInputPacket $packet) : bool {
+		return $this->player->handlePlayerInput($packet);
+	}
+}
