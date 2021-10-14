176f4cdb1be92e4d1274ea8f0d1603344b049423
BalkanDev <75144786+TheBalkanDev@users.noreply.github.com>
2021-10-14 19:34:36 +0200

    Delete src/jasonwynn10/VanillaEntityAI/entity directory

--- src/jasonwynn10/VanillaEntityAI/entity/neutral/DangerousWitherSkull.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace jasonwynn10\VanillaEntityAI\entity\neutral;
-
-class DangerousWitherSkull extends WitherSkull {
-}
--- src/jasonwynn10/VanillaEntityAI/entity/neutral/Trident.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-namespace jasonwynn10\VanillaEntityAI\entity\neutral;
-
-use pocketmine\entity\projectile\Projectile;
-
-class Trident extends Projectile {
-}
